ed65e32979a7c238fa6d2143e2f88dc514e8f677
MoetezBellahFennira <motazzbellahfennira@nabeul.r-iset.tn>
2024-05-13 16:58:20 +0100

    add login confirmation and logo for navBar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import {  Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { InscriptionPopupComponent } from '../inscription-popup/inscription-popup.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -13,50 +14,79 @@ import { InscriptionPopupComponent } from '../inscription-popup/inscription-popu
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+email: any;
+password: any;
   constructor(private userAuthService:UserAuthService,private authConfigService:AuthConfigService,private r:Router,private injector: Injector){}
 
-  openPopup(): void {
-    const dialog = this.injector.get(MatDialog);
-    dialog.open(InscriptionPopupComponent, {
-      width: '800px'
-    });
-  }
+  // openPopup(): void {
+  //   const dialog = this.injector.get(MatDialog);
+  //   dialog.open(InscriptionPopupComponent, {
+  //     width: '800px'
+  //   });
+  // }
   
 
   user!:User;
   
   ngOnInit(){
-    this.openPopup();
+    // this.openPopup();
   }
   
-  login(t7:NgForm){
-   this.user=new User();
-   this.user.email=t7.controls["email"].value;
-   this.user.motDePasse=t7.controls["password"].value;
+  login(t7: NgForm) {
+    if (t7 && t7.valid) {
+      this.user=new User();
+   this.user.email=t7.value.email;
+   this.user.motDePasse=t7.value.password;
    this.userAuthService.login(this.user)
-   .subscribe((data)=>{console.log(data);
-   console.log(data.user);
-   this.authConfigService.setToken(data.token)
-   this.authConfigService.setType(data.user.type)
-  this.authConfigService.setEmail(data.user.email)
-  
-  if(data.user.type=="Coach"){
-    this.r.navigate(['/homeCoach']);
+        .subscribe(
+          (data) => {
+            console.log(data);
+            console.log(data.user);
+            this.authConfigService.setToken(data.token);
+            this.authConfigService.setType(data.user.type);
+            this.authConfigService.setEmail(data.user.email);
+
+            // Redirect based on user type
+            switch (data.user.type) {
+              case "Coach":
+                this.r.navigate(['/homeCoach']);
+                break;
+              case "StaffMedical":
+                this.r.navigate(['/homeMedical']);
+                break;
+              case "StaffPhysique":
+                this.r.navigate(['/homeprepphysique']);
+                break;
+              default:
+                this.r.navigate(['/homeDirecteur']);
+                break;
+            }
+          },
+          (error) => {
+            
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "Login failed: Check Email OR Password!",
+            });
+            
+            console.error("Login failed:", error);
+            // Handle login failure, e.g., display an error message to the user
+          }
+        );
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Form is invalid. Please check the fields.",
+      });
+      
+      console.error("Form is invalid. Please check the fields.");
+      // Handle invalid form submission, e.g., display an error message to the user
+    }
   }
-  else if(data.user.type=="StaffMedical"){
-    this.r.navigate(['/homeMedical']);
-  }
-  else if(data.user.type=="StaffPhysique"){
-    this.r.navigate(['/homeprepphysique']);
-  }
-  else {
-    this.r.navigate(['/homeDirecteur']);
-  }
-   
-   },
-  )
   }
   
   
  
-}
+
